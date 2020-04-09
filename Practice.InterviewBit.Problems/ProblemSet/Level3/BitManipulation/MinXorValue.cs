f46b626f452867a8a96f01335de8b5f831d5a1c4
Yigitalp Ertem <yalpertem@gmail.com>
2020-04-09 18:17:09 +0300

    2 BitManipulation problems added.

---
+++ Practice.InterviewBit.Problems/ProblemSet/Level3/BitManipulation/MinXorValue.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.InterviewBit.Problems.ProblemSet.Level3.BitManipulation
+{
+    /// <summary>
+    /// Min XOR value
+    /// https://www.interviewbit.com/problems/min-xor-value/
+    /// </summary>
+    public class MinXorValue
+    {
+        /*
+        Given an integer array A of N integers, find the pair of integers in the array which have minimum XOR value. Report the minimum XOR value.
+
+        Input Format:
+            First and only argument of input contains an integer array A
+        Output Format:
+            return a single integer denoting minimum xor value
+        
+        Constraints:
+        2 <= N <= 100 000  
+        0 <= A[i] <= 1 000 000 000
+
+        Examples :
+
+        Example Input 1:
+            A = [0, 2, 5, 7]
+        Example Output 1:
+            2
+        Explanation:
+            0 xor 2 = 2
+
+        Example Input 2:
+            A = [0, 4, 7, 9]
+        Example Output 2:
+            3
+        */
+
+        public int Solve(List<int> nums)
+        {
+            nums.Sort();
+            var minXor = int.MaxValue;
+            for (var i = 1; i < nums.Count; i++)
+            {
+                minXor = Math.Min(minXor, nums[i] ^ nums[i - 1]);
+            }
+            return minXor;
+        }
+    }
+}
---
+++ Practice.InterviewBit.Problems/ProblemSet/Level3/BitManipulation/SingleNumber.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.InterviewBit.Problems.ProblemSet.Level3.BitManipulation
+{
+    /// <summary>
+    /// Single Number
+    /// https://www.interviewbit.com/problems/single-number/
+    /// </summary>
+    public class SingleNumber
+    {
+        /*
+        Given an array of integers, every element appears twice except for one. Find that single one.
+        Note: Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+        Input Format:
+            First and only argument of input contains an integer array A
+        Output Format:
+            return a single integer denoting single element
+        
+        Constraints:
+        2 <= N <= 2 000 000  
+        0 <= A[i] <= INT_MAX
+
+        Examples :
+
+        Example Input 1:
+            A = [1, 2, 2, 3, 1]
+        Example Output 1:
+            3
+        Explanation:
+            3 occurs only once
+
+        Example Input 2:
+            A = [1, 2, 2]
+        Example Output 2:
+            1
+        */
+
+        public int Solve(List<int> nums)
+        {
+            var result = 0;
+            for (var i = 0; i < nums.Count; i++)
+            {
+                result ^= nums[i];
+            }
+            return result;
+        }
+    }
+}
