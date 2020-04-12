6fc6a331fece3d5971bd4aa13ade75a6e1193183
Yigitalp Ertem <yalpertem@gmail.com>
2020-04-12 18:52:15 +0300

    3 StacksAndQueues problems added.

---
+++ Practice.InterviewBit.Problems/ProblemSet/Level4/StacksAndQueues/EvaluateExpression.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.InterviewBit.Problems.ProblemSet.Level4.StacksAndQueues
+{
+    /// <summary>
+    /// Evaluate Expression
+    /// https://www.interviewbit.com/problems/evaluate-expression/
+    /// </summary>
+    public class EvaluateExpression
+    {
+        /*
+        Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+        Valid operators are +, -, *, /. Each operand may be an integer or another expression.
+
+        Input Format
+        The only argument given is character array A.
+        Output Format
+        Return the value of arithmetic expression formed using reverse Polish Notation.
+        
+        For Example
+
+        Input 1:
+            A =   ["2", "1", "+", "3", "*"]
+        Output 1:
+            9
+        Explaination 1:
+            starting from backside:
+            *: ( )*( )
+            3: ()*(3)
+            +: ( () + () )*(3)
+            1: ( () + (1) )*(3)
+            2: ( (2) + (1) )*(3)
+            ((2)+(1))*(3) = 9
+    
+        Input 2:
+            A = ["4", "13", "5", "/", "+"]
+        Output 2:
+            6
+        Explaination 2:
+            +: ()+()
+            /: ()+(() / ())
+            5: ()+(() / (5))
+            1: ()+((13) / (5))
+            4: (4)+((13) / (5))
+            (4)+((13) / (5)) = 6
+        */
+
+        public int Solve(List<string> A)
+        {
+            var stack = new Stack<string>();
+
+            for (var i = A.Count - 1; i >= 0; i--)
+            {
+                stack.Push(A[i]);
+                EvalStackIfNecessary(stack);
+            }
+            var result = stack.Pop();
+            return int.Parse(result);
+        }
+
+        private void EvalStackIfNecessary(Stack<string> stack)
+        {
+            while (true)
+            {
+                var stackTop = stack.Peek();
+                if (stackTop == null || IsOperation(stackTop))
+                {
+                    break;
+                }
+
+                var stackTopNum = int.Parse(stack.Pop());
+                var stackSecondTop = stack.Peek();
+                if (stackSecondTop == null || IsOperation(stackSecondTop))
+                {
+                    stack.Push(stackTop);
+                    break;
+                }
+                else
+                {
+                    var stackSecondTopNum = int.Parse(stack.Pop());
+                    var stackOp = stack.Pop();
+                    var currentResult = EvalSingle(stackTopNum,
+                        stackSecondTopNum, stackOp);
+                    stack.Push(currentResult.ToString());
+                }
+            }
+
+        }
+
+        private bool IsOperation(string s)
+        {
+            return s == "+" || s == "-" || s == "*" || s == "/";
+        }
+
+        private int EvalSingle(int num1, int num2, string operation)
+        {
+            return operation switch
+            {
+                "+" => num1 + num2,
+                "-" => num1 - num2,
+                "/" => num1 / num2,
+                "*" => num1 * num2,
+                _ => 0,
+            };
+        }
+    }
+}
---
+++ Practice.InterviewBit.Problems/ProblemSet/Level4/StacksAndQueues/SimplifyDirectoryPath.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.InterviewBit.Problems.ProblemSet.Level4.StacksAndQueues
+{
+    /// <summary>
+    /// Simplify Directory Path
+    /// https://www.interviewbit.com/problems/simplify-directory-path/
+    /// </summary>
+    public class SimplifyDirectoryPath
+    {
+        /*
+        Given a string A representing an absolute path for a file (Unix-style).
+        Return the string A after simplifying the absolute path.
+
+        Note:
+        Absolute path always begin with ’/’ ( root directory ).
+        Path will not have whitespace characters.
+
+        Input Format
+        The only argument given is string A.
+        Output Format
+        Return a string denoting the simplified absolue path for a file (Unix-style).
+
+        Examples
+
+        Input 1:
+            A = "/home/"
+        Output 1:
+            "/home"
+
+        Input 2:
+            A = "/a/./b/../../c/"
+        Output 2:
+            "/c"
+        */
+
+        public string Solve(string str)
+        {
+            var splittedPath = str.Split('/');
+            var stack = new Stack<string>();
+            for (var i = 0; i < splittedPath.Length; i++)
+            {
+                var part = splittedPath[i];
+                if (part == "" || part == ".")
+                {
+                    continue;
+                }
+                else if (part == "..")
+                {
+                    if (stack.Count != 0)
+                    {
+                        stack.Pop();
+                    }
+                }
+                else
+                {
+                    stack.Push(part);
+                }
+            }
+
+            if (stack.Count == 0)
+            {
+                return "/";
+            }
+
+            var reverseStack = new Stack<string>();
+            while (stack.Count > 0)
+            {
+                reverseStack.Push(stack.Pop());
+            }
+
+            var sb = new StringBuilder("/");
+            while (reverseStack.Count > 0)
+            {
+                sb.Append("/");
+                sb.Append(reverseStack.Pop());
+            }
+            return sb.ToString();
+        }
+    }
+}
