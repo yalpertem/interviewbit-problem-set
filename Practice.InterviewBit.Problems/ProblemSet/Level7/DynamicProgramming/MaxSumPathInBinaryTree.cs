c7152463b16e484ad89156ae7b3e0179a6e3aab2
Yigitalp Ertem <yalpertem@gmail.com>
2020-04-14 15:04:18 +0300

    2  DynamicProgramming problems added.

---
+++ Practice.InterviewBit.Problems/ProblemSet/Level7/DynamicProgramming/MaxSumPathInBinaryTree.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.InterviewBit.Problems.ProblemSet.Level7.DynamicProgramming
+{
+    /// <summary>
+    /// Max Sum Path in Binary Tree
+    /// https://www.interviewbit.com/problems/max-sum-path-in-binary-tree/
+    /// </summary>
+    public class MaxSumPathInBinaryTree
+    {
+        /*
+        Given a binary tree T, find the maximum path sum.
+        The path may start and end at any node in the tree.
+
+        Input Format:
+        The first and the only argument contains a pointer to the root of T, A.
+        Output Format:
+        Return an integer representing the maximum sum path.
+        Constraints:
+        1 <= Number of Nodes <= 7e4
+        -1000 <= Value of Node in T <= 1000
+        
+        Example :
+
+        Input 1:
+               1
+              / \
+             2   3
+        Output 1:
+             6
+        Explanation 1:
+        The path with maximum sum is: 2 -> 1 -> 3
+
+        Input 2:
+               -10
+               /  \
+             -20  -30
+        Output 2:
+            -10
+        Explanation 2:
+        The path with maximum sum is: -10
+        */
+
+        private int _max;
+        public int Solve(TreeNode head)
+        {
+            _max = int.MinValue;
+            if (head == null)
+            {
+                return 0;
+            }
+            GetCurrentPathSum(head);
+            return _max;
+        }
+
+        private int GetCurrentPathSum(TreeNode head)
+        {
+            if (head == null)
+            {
+                return 0;
+            }
+
+            var currentMax = head.val;
+            var leftPathSum = GetCurrentPathSum(head.left);
+            var rightPathSum = GetCurrentPathSum(head.right);
+            if (leftPathSum > 0)
+            {
+                currentMax += leftPathSum;
+            }
+            if (rightPathSum > 0)
+            {
+                currentMax += rightPathSum;
+            }
+            _max = Math.Max(_max, currentMax);
+
+            return leftPathSum > rightPathSum ?
+                head.val + Math.Max(0, leftPathSum) :
+                head.val + Math.Max(0, rightPathSum);
+        }
+
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int x) { this.val = x; this.left = this.right = null; }
+        }
+    }
+}
---
+++ Practice.InterviewBit.Problems/ProblemSet/Level7/DynamicProgramming/UniqueBinarySearchTreesII.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.InterviewBit.Problems.ProblemSet.Level7.DynamicProgramming
+{
+    /// <summary>
+    /// Unique Binary Search Trees II
+    /// https://www.interviewbit.com/problems/unique-binary-search-trees-ii/
+    /// </summary>
+    public class UniqueBinarySearchTreesII
+    {
+        /*
+        Given an integer A, how many structurally unique BST’s (binary search trees) exist that can store values 1…A?
+
+        Input Format:
+        The first and the only argument of input contains the integer, A.
+        
+        Output Format:
+        Return an integer, representing the answer asked in problem statement.
+        
+        Constraints:
+        1 <= A <= 18
+        
+        Example:
+        Input 1:
+            A = 3
+        Output 1:
+            5
+        Explanation 1:
+           1         3     3      2      1
+            \       /     /      / \      \
+             3     2     1      1   3      2
+            /     /       \                 \
+           2     1         2                 3
+
+        */
+
+        public int Solve(int A)
+        {
+            var dp = CreateDP(A);
+            var result = GetNumTreesDP(A, dp);
+            return result;
+        }
+
+        private int[] CreateDP(int A)
+        {
+            var dp = new int[A + 1];
+            dp[0] = 1;
+            dp[1] = 1;
+            for (var i = 2; i <= A; i++)
+            {
+                dp[i] = -1;
+            }
+            return dp;
+        }
+
+        private int GetNumTreesDP(int A, int[] dp)
+        {
+            if (dp[A] != -1)
+            {
+                return dp[A];
+            }
+
+            var total = 0;
+            for (var i = 0; i < A; i++)
+            {
+                var left = GetNumTreesDP(A - i - 1, dp);
+                var right = GetNumTreesDP(i, dp);
+                total += left * right;
+            }
+            dp[A] = total;
+            return total;
+        }
+    }
+}
