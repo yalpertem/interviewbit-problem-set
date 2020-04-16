7c32c536ab9da1e4b61097b6287549a2e2f80cbf
Yigitalp Ertem <yalpertem@gmail.com>
2020-04-16 16:24:57 +0300

    LargestRectangleInHistogram problem added.

---
+++ Practice.InterviewBit.Problems.Tests/ProblemSet/Level4/StacksAndQueues/LargestRectangleInHistogramTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Practice.InterviewBit.Problems.ProblemSet.Level4.StacksAndQueues;
+using Practice.InterviewBit.Problems.ProblemSet.Level7.DynamicProgramming;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.InterviewBit.Problems.Tests.ProblemSet.Level4.StacksAndQueues
+{
+    [TestClass]
+    public class LargestRectangleInHistogramTests
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            var problem = new LargestRectangleInHistogram();
+            var input = new List<int> { 2, 1, 5, 6, 2, 3 };
+            var expectedOutput = 10;
+            var output = problem.Solve(input);
+            Assert.AreEqual(expectedOutput, output);
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            var problem = new LargestRectangleInHistogram();
+            var input = new List<int> { 12, 43, 65, 76, 84, 29, 95, 12, 86, 93, 100, 34, 54, 92, 82, 21, 43 };
+            var expectedOutput = 258;
+            var output = problem.Solve(input);
+            Assert.AreEqual(expectedOutput, output);
+        }
+    }
+}
---
+++ Practice.InterviewBit.Problems/ProblemSet/Level4/StacksAndQueues/LargestRectangleInHistogram.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.InterviewBit.Problems.ProblemSet.Level4.StacksAndQueues
+{
+    /// <summary>
+    /// Largest Rectangle in Histogram
+    /// https://www.interviewbit.com/problems/largest-rectangle-in-histogram/
+    /// </summary>
+    public class LargestRectangleInHistogram
+    {
+        /*
+        Given an array of integers A of size N. A represents a histogram i.e A[i] denotes height of
+        the ith histogram’s bar. Width of each bar is 1.
+        Above is a histogram where width of each bar is 1, given height = [2,1,5,6,2,3].
+        The largest rectangle is shown in the shaded area, which has area = 10 unit.
+        Find the area of largest rectangle in the histogram.
+
+        Input Format
+        The only argument given is the integer array A.
+        Output Format
+        Return the area of largest rectangle in the histogram.
+        
+        For Example
+        Input 1:
+            A = [2, 1, 5, 6, 2, 3]
+        Output 1:
+            10
+        Explanation 1:
+        The largest rectangle is shown in the shaded area, which has area = 10 unit.
+        */
+
+        public int Solve(List<int> bars)
+        {
+            var stack = new Stack<int>();
+            var max = 0;
+            for (var i = 0; i < bars.Count; i++)
+            {
+                var num = bars[i];
+                while (stack.Count > 0 && bars[stack.Peek()] > num)
+                {
+                    var curMax = CalculateArea(bars, stack, i);
+                    max = Math.Max(max, curMax);
+                }
+                stack.Push(i);
+            }
+
+            while (stack.Count > 0)
+            {
+                int curMax = CalculateArea(bars, stack, bars.Count);
+                max = Math.Max(max, curMax);
+            }
+            return max;
+        }
+
+        private static int CalculateArea(List<int> bars, Stack<int> stack, int i)
+        {
+            var old = stack.Pop();
+            var length = stack.Count == 0 ? i : (i - stack.Peek() - 1);
+            var curMax = bars[old] * length;
+            return curMax;
+        }
+    }
+}
